5f7999d064a034184679daf61ff39eab274fc936
Keshavgarg30 <gargkeshav56@gmail.com>
2022-08-16 14:21:24 +0530

    added files

---
+++ script.js
@@ -0,0 +1,22 @@
+const xhr = new XMLHttpRequest();
+const source =
+  "https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09";
+xhr.open("GET", source, true);
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const response = JSON.parse(xhr.responseText);
+    let output = "";
+    for (let i = 0; i < 100; i++) {
+      output += `
+        <div class="box">
+            <div class="content">
+                <p class="user">User - ${response[i].userId}</p>
+                <p class="title">${response[i].title}</p>
+                <p class="body">${response[i].body}</p>
+            </div>
+        </div>`;
+    }
+    document.querySelector(".container").innerHTML = output;
+  }
+};
+xhr.send();
